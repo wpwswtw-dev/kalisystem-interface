58cf38069b606a75a1082f2a2aff8581ea21622d
wpwswtw-dev <wpwswtw@outlook.com>
2025-10-19 10:56:29 +0000

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: "0.0.0.0",
     port: 5000,
@@ -17,5 +19,10 @@ export default defineConfig(({ mode }) => ({
     host: "0.0.0.0",
     port: 5000,
     strictPort: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 }));
